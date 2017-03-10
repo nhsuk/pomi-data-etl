2865f990826bc54ae1b79537e0fb88b8e1cc6c77
steven hunt <st3v3n.hunt@gmail.com>
2017-03-10 12:47:02 +0000

    :godmode: Increase test coverage

--- app/lib/apiRequest.js
+++ app/lib/apiRequest.js
@@ -8,7 +8,7 @@ function apiRequest(url) {
     const request = lib.get(url, (response) => {
       // handle http errors
       if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error(`Failed to load page, status code:  ${response.statusCode}`));
+        reject(new Error(`Failed to load page, status code: ${response.statusCode}`));
       }
       // temporary data holder
       const body = [];
