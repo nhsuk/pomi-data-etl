ffe3dcfb1c8a5a1a734098d815b47f64f036a6e6
steven hunt <st3v3n.hunt@gmail.com>
2017-03-10 14:12:27 +0000

    :point_left: Simplify promise handling

--- app/lib/downloadFile.js
+++ app/lib/downloadFile.js
@@ -7,19 +7,12 @@ const POMI_FILE = constants.POMI_FILE;
 const POMI_DOWNLOAD_LOCATION = constants.POMI_DOWNLOAD_LOCATION;
 
 function downloadFile() {
-  return new Promise((resolve, reject) => {
-    try {
-      log.time('Downloading the POMI file took');
-      resolve(
-        apiRequest(POMI_DOWNLOAD_LOCATION)
-        .then((data) => {
-          log.timeEnd('Downloading the POMI file took');
-          fsHelper.saveFileSync(data, POMI_FILE);
-        }));
-    } catch (err) {
-      reject(err);
-    }
-  });
+  log.time('Downloading the POMI file took');
+  return apiRequest(POMI_DOWNLOAD_LOCATION)
+    .then((data) => {
+      log.timeEnd('Downloading the POMI file took');
+      fsHelper.saveFileSync(data, POMI_FILE);
+    });
 }
 
 module.exports = downloadFile;
--- app/lib/getLatestPeriod.js
+++ app/lib/getLatestPeriod.js
@@ -2,18 +2,12 @@ const log = require('./logger');
 const utils = require('./utils');
 
 function getLatestPeriod(periods) {
-  return new Promise((resolve, reject) => {
-    try {
-      log.time('Getting latest period took');
-      const latestPeriod = utils.getMostRecentPeriod(periods);
-      log.timeEnd('Getting latest period took');
-      log.info(`Latest period is ${latestPeriod}`);
+  log.time('Getting latest period took');
+  const latestPeriod = utils.getMostRecentPeriod(periods);
+  log.timeEnd('Getting latest period took');
+  log.info(`Latest period is ${latestPeriod}`);
 
-      resolve(latestPeriod);
-    } catch (err) {
-      reject(err);
-    }
-  });
+  return latestPeriod;
 }
 
 module.exports = getLatestPeriod;
