e35ca64b3236d6f62b65d670189fe8388a76c2f0
steven hunt <st3v3n.hunt@gmail.com>
2017-04-24 15:33:52 +0100

    :notebook: Add detailed coded records for processing

--- runner.js
+++ runner.js
@@ -2,6 +2,14 @@ const app = require('./app');
 
 const bookingDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/BOOK_CANCEL_APPOINTMENTS_POMI.csv';
 const scriptsDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/ORDER_REPEAT_PRESCRIPTIONS_POMI.csv';
+const codedRecordsDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/DETAILED_CODED_RECORDS_POMI.csv';
 
-app.downloadAndProcessFile({ type: 'SCRIPTS', url: scriptsDownloadUrl });
-app.downloadAndProcessFile({ type: 'BOOKING', url: bookingDownloadUrl });
+const downloads = [
+  { type: 'BOOKING', url: bookingDownloadUrl },
+  { type: 'SCRIPTS', url: scriptsDownloadUrl },
+  { type: 'RECORDS', url: codedRecordsDownloadUrl },
+];
+
+downloads.forEach((request) => {
+  app.downloadAndProcessFile(request);
+});
