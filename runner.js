8ff2df2f608f47e7a63466aef6fbcad57f6e3b35
steven hunt <st3v3n.hunt@gmail.com>
2017-04-24 11:34:41 +0100

    :bug: Correct name of repeat prescription file

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const bookingDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/BOOK_CANCEL_APPOINTMENTS_POMI.csv';
-const scriptsDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/ORDER_REPEAT_PRESCRIPTION_POMI.csv';
+const scriptsDownloadUrl = 'https://indicators.hscic.gov.uk/download/PHF10/Data/ORDER_REPEAT_PRESCRIPTIONS_POMI.csv';
 
 app.downloadAndProcessFile({ type: 'SCRIPTS', url: scriptsDownloadUrl });
 app.downloadAndProcessFile({ type: 'BOOKING', url: bookingDownloadUrl });
